4bf87204f55763a8ad15d8e5718fac66cee790c2
Akari <hetors.email@gmail.com>
2011-07-16 14:04:13 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+
+from distutils.core import setup
+
+setup(
+		name='dprcon',
+		version='1.0',
+		description='A simple DarkPlaces RCON client',
+		author='Akari`',
+		author_email='hetors.email@gmail.com',
+		py_modules = ['dprcon']
+)
