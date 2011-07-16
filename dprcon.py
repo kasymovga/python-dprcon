a7533f970f65e57bf05779bd486b44f1cd648e7c
Akari <hetors.email@gmail.com>
2011-07-16 17:21:25 +0300

    Implemented ChallengeBasedSecureRCONConnection (rcon_secure 2)

--- dprcon.py
+++ dprcon.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2
 
-import socket, re, sys, md4, hmac, random, time
+import socket, re, sys, md4, hmac, random, time, select
 
 class RCONException(Exception):
 	pass
@@ -9,6 +9,9 @@ class RCONConnectionRequiredException(RCONException):
 	pass
 
 class RCONAlreadyConnectedException(RCONException):
+	pass
+
+class RCONChallengeTimeoutException(RCONException):
 	pass
 
 def requireConnected(f):
@@ -32,6 +35,7 @@ def requireDisconnected(f):
 	return wrapper 
 
 responseRegexp = re.compile("\377\377\377n(.*)", re.S)
+challengeRegexp = re.compile("\377\377\377\377challenge (.*?)(?:$|\0)", re.S)
 
 class InsecureRCONConnection(object):
 	def __init__(self, host, port, password, connect=False, bufsize=1024):
@@ -100,6 +104,56 @@ class TimeBasedSecureRCONConnection(InsecureRCONConnection):
 			mytime, line
 		)
 
+class ChallengeBasedSecureRCONConnection(InsecureRCONConnection):
+	def __init__(self, host, port, password, connect=False, bufsize=1024, timeout=3):
+		self.challengeTimeout = timeout
+		self.recvbuf = []
+		self._challenge = ""
+		
+		return super(ChallengeBasedSecureRCONConnection, self).__init__(host, port, password, connect, bufsize)
+	
+	def send(self, *s):
+		self._challenge = self._recvchallenge()
+		return super(ChallengeBasedSecureRCONConnection, self).send(*s)
+	
+	def makeRCONMessage(self, line):
+		return "\377\377\377\377srcon HMAC-MD4 CHALLENGE %s %s %s" %(
+			hmac.new(self._pwd, "%s %s" % (self._challenge, line), digestmod=md4.new).digest(),
+			self._challenge, line
+		)
+	
+	def translateChallengeResponse(self, s):
+		try:
+			return challengeRegexp.findall(s)[0]
+		except IndexError:
+			return ""
+		
+	def _recvchallenge(self):
+		self._sock.send("\377\377\377\377getchallenge");
+		timeouttime = time.time() + self.challengeTimeout
+		
+		while time.time() < timeouttime:
+			r, w, x = select.select([self._sock], [], [])
+			
+			if self._sock in r:
+				s = self._sock.recv(self.bufsize)
+				
+				r = self.translateRCONResponse(s)
+				if r:
+					self.recvbuf.append(r)
+				else:
+					c = self.translateChallengeResponse(s)
+					print c
+					if c:
+						return c
+		
+		raise RCONChallengeTimeoutException
+	
+	def read(self, bufsize=None):
+		if self.recvbuf:
+			return self.recvbuf.pop(0)
+		return super(ChallengeBasedSecureRCONConnection, self).read(bufsize)
+
 if __name__ == "__main__":
 	host = raw_input("Server: ")
 	port = int(raw_input("Port: "))
@@ -109,7 +163,8 @@ if __name__ == "__main__":
 	try:
 		rcon = {
 			0:	InsecureRCONConnection,
-			1:	TimeBasedSecureRCONConnection
+			1:	TimeBasedSecureRCONConnection,
+			2:	ChallengeBasedSecureRCONConnection
 		}[sec](host, port, pwd, connect=True)
 	except KeyError as e:
 		print "Invalid security value:", sec
@@ -118,6 +173,7 @@ if __name__ == "__main__":
 	print "Connected!"
 	
 	rcon.getSocket().settimeout(1)
+	print "Local address:", rcon.getLocalAddress()
 	
 	while True:
 		rcon.send(raw_input("Command: "))
